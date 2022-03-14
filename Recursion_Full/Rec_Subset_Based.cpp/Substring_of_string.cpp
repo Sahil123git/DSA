b88ee91e86a6fae020b837aa6f7e6076aa724d8d
sahil_1123 <59478266+Sahil123git@users.noreply.github.com>
2022-03-14 13:37:12 +0530

    Add files via upload

---
+++ Recursion_Full/Rec_Subset_Based.cpp/Substring_of_string.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Function to print all sub strings
+void subString(string s, int n)
+{
+    // outer loop will inc
+    // inner loop will dec for every inc outer loop
+    for (int i = 0; i < n; i++)
+        for (int len = n; len > i; len--)
+            cout << s.substr(i, len) << endl;
+}
+
+// Driver program to test above function
+int main()
+{
+    string s = "abcd";
+    subString(s, s.length());
+    return 0;
+}
