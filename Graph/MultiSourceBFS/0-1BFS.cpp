b6c8b2c7b3bf64a5b1ace0f4710ffc896e6db100
sahil_1123 <59478266+Sahil123git@users.noreply.github.com>
2022-07-20 16:41:58 +0530

    Update 0-1BFS.cpp

--- Graph/MultiSourceBFS/0-1BFS.cpp
+++ Graph/MultiSourceBFS/0-1BFS.cpp
@@ -13,27 +13,25 @@ void BFS_0_1()
 {
     deque<int> dq;
     dq.push_back(1);
-    lvl[1] = 0;
+    lvl[1] = 0; //source node wgt is 0
 
     while (!dq.empty()) // this algo can also be used for shortest path from source to other nodes(Like Dijkstra Algo), but this will chk edge multiple times which can inc the TC
     {
         int curEle = dq.front();
-        dq.pop_front();
-        for (auto nbr : gr[curEle])
+        dq.pop_front(); //remove chkd node from queue
+        for (auto nbr : gr[curEle]) //chkng nbrs of queue front node
         {
             int ele = nbr.first;
             int wt = nbr.second;
-            if (lvl[curEle] + wt < lvl[ele])
+            if (lvl[curEle] + wt < lvl[ele]) //if adding this wt dec prev wgt then only update.
             {
                 lvl[ele] = lvl[curEle] + wt;
-                if (wt == 1)
+                if (wt == 1) //if wgt of edge is 1 means insert at the end of queue
                 {
-                    // cout << "now here";
                     dq.push_back(ele);
                 }
                 else if (wt == 0) // 0 is like lowest wt so insert at the front
                 {
-                    // cout << "here";
                     dq.push_front(ele);
                 }
             }
@@ -47,7 +45,7 @@ int main()
 
     gr = new vector<pair<int, int>>[n + 1];
 
-    for (int i = 0; i < m; i++)
+    for (int i = 0; i < m; i++) //nodes can be 1 to n
     {
         int x, y, wt;
         cin >> x >> y >> wt;
@@ -61,7 +59,7 @@ int main()
     }
     BFS_0_1();
 
-    for (int i = 1; i <= n; i++)
+    for (int i = 1; i <= n; i++) //lowest wgt from source to all other nodes
     {
         cout << lvl[i] << " ";
     }
