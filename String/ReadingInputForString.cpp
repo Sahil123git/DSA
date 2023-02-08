8dabeb61944cdb1cee402f1b61c60a2edfdeaef3
sahil_1123 <59478266+Sahil123git@users.noreply.github.com>
2023-02-08 21:40:05 +0530

    Add files via upload

---
+++ String/ReadingInputForString.cpp
@@ -0,0 +1,23 @@
+// Note: Think twice, First Instinct that come to ur mind tht could be helpful
+#include <bits/stdc++.h>
+using namespace std;
+void yes_i_will_solve()
+{
+    string str;
+    getline(cin, str); // this will take whole line as input
+
+    cout << str;
+}
+
+int32_t main()
+{
+    int t;
+    cin >> t;
+    cin.ignore(); // use this so tht you can use getline()
+    while (t--)
+    {
+        yes_i_will_solve();
+        cout << "\n";
+    }
+    return 0;
+}
